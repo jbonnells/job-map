267b50cdca477edc112cd2a15b464e2eb60ca74c
Julian Bonnells <17135680+jbonnells@users.noreply.github.com>
2025-02-07 10:59:49 -0700

    add env template file and update run.sh to check for existence

--- run.sh
+++ run.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 set -e
 
+# Create .env from template if it doesn't exist
+if [ ! -f .env ]; then
+    cp .env.template .env
+    echo "Please populate the .env file with your API keys."
+fi
+
 # Activate virtual environment
 source venv/Scripts/activate
 
